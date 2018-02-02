aa934a1805f8da296d7d168c4ec019f10812c017
Benjamin Ross <benjaminross1001@gmail.com>
2018-02-01 18:31:37 -0700

    server config and refactoring

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-const express = require("express");
+import express from "express";
+import renderer from './helpers/renderer'
+
 const app = express();
 
-app.get("/", (req, res) => {});
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  
+  res.send(renderer());
+});
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
